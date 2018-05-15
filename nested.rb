e4e6a20bc0fd5b33b32eb1b8b55285844ba42b83
Precious Humphrey <phumphrey23@gmail.com>
2018-05-15 00:22:43 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 adding_to_dennis[:dennis_ritchie][:languages][1] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 return programmer_hash
 end
