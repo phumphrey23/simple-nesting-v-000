66e0e3a5276ecbcee05882c8162b0ce39ea289d2
Precious Humphrey <phumphrey23@gmail.com>
2018-05-15 00:42:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:language]
+		 return programmer_hash[:dennis_ritchie][:language][0]
 end
 
 def adding_matz
